c697db94117b2d30f090c87f4fbd440992b86424
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-10 15:32:51 +0530

    Added Even or Odd program

---
+++ src/com/BasicJavaPrograms/EvenOrOdd.java
@@ -0,0 +1,26 @@
+package com.BasicJavaPrograms;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		int N;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Please enter a Number");
+			while(!sc.hasNextInt()) {
+				System.out.println("Entered Input is not a number");
+				sc.next();
+			}
+			N = sc.nextInt();
+			break;
+		}while(true);
+		sc.close();
+		
+		if(N % 2 ==0)
+			System.out.println("Even Number");
+		else
+			System.out.println("odd Number");
+
+}
+}
