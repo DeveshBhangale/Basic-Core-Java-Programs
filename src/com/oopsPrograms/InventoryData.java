e2271eade69f6acd57a173a0e939e3aae340ed88
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-18 01:26:18 +0530

    [Devesh]: Completed Inventory Data

---
+++ src/com/oopsPrograms/InventoryData.java
@@ -0,0 +1,30 @@
+package com.oopsPrograms;
+
+
+class InventoryData {
+	public final String name;
+	public final double weight;
+	public final double price;
+	
+	public InventoryData(String name,double weight,double price) {
+		this.name = name;
+		this.weight = weight;
+		this.price = price;
+	}	
+	
+	public void calculate() {
+		System.out.printf("Total price of "+name+" "+weight+" kg is "+"%.2f Rs \n",(weight*price));
+	}
+	
+	public static void main(String[] args) {
+		InventoryData rice = new InventoryData("Rice",2,60);
+		InventoryData pulses = new InventoryData("pulses",5.2,100.5);
+		InventoryData wheats = new InventoryData("wheats",8.5,55.8);
+		rice.calculate();
+		pulses.calculate();
+		wheats.calculate();
+	}
+
+
+
+}
