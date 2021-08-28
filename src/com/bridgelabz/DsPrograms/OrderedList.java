d99b7eac3541a4762c02042ed5504a5c6a0ede3a
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-28 15:30:04 +0530

    [Devesh]: Completed Ordered List

--- src/com/bridgelabz/DsPrograms/OrderedList.java
+++ src/com/bridgelabz/DsPrograms/OrderedList.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.DsPrograms;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
-import java.util.*;
-
-
-public class UnOrderedList<K> {
+public class OrderedList<K> {
+	
 	private INode<K> head;
 	private INode<K> tail;
 	
@@ -48,12 +50,14 @@ public class UnOrderedList<K> {
 		INode<K> tempNode = head;
 		if(tempNode.getKey().equals(myNode)) {
 			System.out.println(myNode + "is found");
+			
 		}
 		else {
 			while(tempNode != null) {
 				if(tempNode.getKey().equals(myNode)) {
 					System.out.println(myNode + " is found");
 					break;
+					
 				}
 				tempNode = tempNode.getNext();
 			}
@@ -80,11 +84,57 @@ public class UnOrderedList<K> {
 		System.out.println(str);
 	}
 	
+	public int size() {
+		int i = 0;
+		INode<K> tempNode = this.head;
+		while(tempNode != null) {
+			tempNode = tempNode.getNext();
+			i++;
+		}
+		return i;
+	}
+	
+	public static String[] sortArray(String arr[]) {
+		int size = arr.length;
+		String temp;
+		for(int i=0;i<size-1;i++) {
+			for (int j=i+1;j<arr.length;j++) {
+				if(arr[i].compareTo(arr[j])>0) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	
+	
+	public void sort() {
+		String arr[] = new String[(int)size()];
+		int i = 0;
+		INode<K> tempNode = this.head;
+		while(tempNode != null) {
+			arr[i] = (String)tempNode.getKey();
+			i++;
+			tempNode = tempNode.getNext();
+		}
+		arr = sortArray(arr);
+		head = null;
+		tail = null;
+		int size = arr.length;
+		for(int j=0;j<size;j++) {
+			add(new MyNode(arr[j]));
+		}
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		File file = new File("C:\\text.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		UnOrderedList<String> l1 = new UnOrderedList<>();
+		OrderedList<String> l1 = new OrderedList<>();
 		String st;
 		while ((st = br.readLine()) != null) {
 				String[] words = st.toLowerCase().split(" ");
@@ -96,7 +146,9 @@ public class UnOrderedList<K> {
 			
 		}
 		l1.print();
-		
+		System.out.println(l1.size());
+		l1.sort();
+		l1.print();
 		System.out.println("Enter a word to delete from list: ");
 		String node = sc.next();
 		l1.delete(node);
@@ -107,5 +159,4 @@ public class UnOrderedList<K> {
 		sc.close();
 		br.close();
 		}
-
 }
